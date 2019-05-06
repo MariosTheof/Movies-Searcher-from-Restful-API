a805ccb2a61c9ee26a14ed666371934a6ef13ae9
Gmaz <mazarakisg@gnt.gr>
2019-05-06 17:18:01 +0300

    Added all required content responses from the API. Function for plot summary has problem return JSON.

--- script.js
+++ script.js
@@ -1,46 +1,79 @@
-/*
-  TODO Το search option του API δεν επιστρεφει το plot, αρα θα πρέπει να κληθεί
-  το search by title για καθε ταινία για να έχουμε το plot.
-
-*/
 window.onload = function(){
     document.getElementById("submit").onclick = function(){
       var title = document.getElementById("movieTitle").value;
-      // document.getElementById("outputName").innerText = title;
 
       var url = "https://www.omdbapi.com/?s=" + title +"&page=5&apikey=640d2cde"
+
+
       const Http = new XMLHttpRequest();
       Http.open("GET", url);
       Http.send();
       Http.onreadystatechange=(e)=>{
-        //console.log(Http.responseText)
         var parsedJson = JSON.parse(Http.responseText);
-        // console.log(parsedJson.Search[0].Title);
-        // console.log(parsedJson.Search[0].Poster);
-        document.getElementById("titleShow").innerText = parsedJson.Search[0].Title;
-        document.getElementById("poster").src = parsedJson.Search[0].Poster;
+
+        for (var i = 0; i < 10; i++){
+          document.getElementById("titleShow"+ i + "").innerText = parsedJson.Search[i].Title;
+          document.getElementById("poster" + i + "").src = parsedJson.Search[i].Poster;
+          var plot = getPlotSummary(parsedJson.Search[i].imdbID);
+
+          document.getElementById("text" + i + "").innerText = plot;
+        }
       }
+
+      for (var i = 0; i < 10; i++){
+        $("#myContainer").append(
+          $('<div/>').addClass("wrapper")
+             .append(
+               $('<div/>').addClass("content")
+                 .append(
+                   $("<figure/>")
+                     .addClass("myimg")
+                     .append(
+                       $('<img src="http://placehold.it/250x200"/>')
+                       .attr("id","poster" + i + "")
+                     )
+
+                 )
+                 .append($("<h3/>").attr("id", "titleShow"+ i + ""))
+                 .append($("<p/>").attr("id", "text"+i+""))    //εδώ να επιστρέφει την περιγραφή
+             )
+        )
+
+      }
+
     }
 
 }
-$(document).ready(function(){
-     $("#submit").click(function(){
-       $("#myContainer").append(
-         $('<div/>').addClass("wrapper")
-            .append(
-              $('<div/>').addClass("content")
-                .append(
-                  $("<figure/>")
-                    .addClass("myimg")
-                    .append(
-                      $('<img src="http://placehold.it/250x200"/>')
-                      .attr("id","poster")
-                    )
 
-                )
-                .append($("<h3/>").attr("id", "titleShow"))
-                // .append($("<p/>").attr()) εδώ να επιστρέφει την περιγραφή
-            )
-       )
-     })
- });
+// Δεν επιστρέφει το json.Plot, μάλλον επειδή μπλέκουμε ασυγχονα calls
+function getPlotSummary(id){
+  var plotSummary = "";
+  var urlForPlotSearches = "https://www.omdbapi.com/?i=" + id + "&apikey=640d2cde"
+  $.getJSON( urlForPlotSearches, function( json ) {
+    console.log("success");
+  })
+  .done( function( json ) {
+    plotSummary = json.Plot;
+    console.log("greater success");
+    console.log( json.Plot );
+    return plotSummary;
+  })
+  .fail( function() {
+    console.log("fail");
+  })
+
+  return "I return nothing"; // change it to null later
+}
+
+/* Returns the plot summaries of a search page
+function getPlotSummaries(parsedJson) {
+  let plotSummaries = new Array(10);
+  for (var i = 0; i < 10; i++){
+    var urlForPlotSearches = "https://www.omdbapi.com/?i=" + parsedJson.Search[i].imdbID + "&apikey=640d2cde"
+    $.getJSON( urlForPlotSearches, function( json ) {
+      plotSummaries[i] = json.Plot;
+    })
+  }
+  return plotSummaries;
+}
+*/
