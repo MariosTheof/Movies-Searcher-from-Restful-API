7725d0c4a8c53c0c036794533fb6f68b9e79054d
Marios Athanasiadis <MariosTheof@users.noreply.github.com>
2019-06-08 19:51:25 +0300

    Improved css, added multiple page navigation, fixed bugs.

--- script.js
+++ script.js
@@ -1,139 +1,210 @@
-window.onload = function(){
-    var hasSearched = false;
+var page=1;
 
+window.onload = function()
+{
+    var hasSearched = false;
     var typingTimer;                //timer identifier
-    var doneTypingInterval = 3000;  //time in ms (5 seconds)
+    var doneTypingInterval = 1800;  //time in ms (3 seconds)
     var userInput = document.getElementById('submit');
 
+
     userInput.addEventListener('keyup', () => {
-      clearTimeout(typingTimer);
-      if (userInput.value) {
-          typingTimer = setTimeout(doneTyping, doneTypingInterval);
-      }
+		clearTimeout(typingTimer);
+
+		if (userInput.value )
+		{
+			typingTimer = setTimeout(doneTyping, doneTypingInterval);
+			document.getElementById('myContainer').innerHTML='';
+			document.getElementById('footer').innerHTML='';
+		}
+		else
+		{
+			document.getElementById('myContainer').innerHTML='<img id="wallpaper" src="https://boygeniusreport.files.wordpress.com/2016/03/movies-tiles.jpg?quality=98&strip=all">';
+			document.getElementById('footer').innerHTML='<p id="team">© Marios & Andromachi 2019</p>';
+			hasSearched = false;
+		}
     });
 
-    function doneTyping() {
 
-      addResponseToDivsInFrontEnd(hasSearched);
-      restoreButtonsIfGone();
-      hasSearched = true;
+    function doneTyping(p)
+	{
+    page = 1;
+		// console.log(page);
+		addResponseToDivsInFrontEnd(hasSearched);
+		restoreButtonsIfGone();
+		//hasSearched = true;
 
-      var title = document.getElementById("submit").value;
+		var title = document.getElementById("submit").value;
+		document.getElementById('footer').innerHTML ='<button type="button" class=button id="previous">Previous</button> <span id="page"> </span><button type="button" class=button id="next">Next</button>';
 
-      var urlForTitleAndPoster = "https://www.omdbapi.com/?s=" + title +"&page=5&apikey=640d2cde";
+		search(title);
 
-      // Retrieve json query
-      var result = getData(urlForTitleAndPoster)
-        .then( function(data) {
-          console.log(data);
-          var releasedClasses = document.getElementsByClassName("released");
-          var titleShowClasses = document.getElementsByClassName("titleShow");
-          var posterClasses = document.getElementsByClassName("poster");
+		var butP = document.getElementById('previous');
 
-          for (var i = 0; i < 10; i++){
-            // Add Title and Poster to html
-            titleShowClasses[i].innerText = data.Search[i].Title;
-            posterClasses[i].src = data.Search[i].Poster;
-            releasedClasses[i].innerText = "Released : " + data.Search[i].Year;
+		butP.addEventListener("click", function() {if (page>1) { page--; restoreButtonsIfGone();search(title);} }	);
 
-          }
-          return data;
-        });
+		var butN = document.getElementById('next');
 
-        // Retrieve plot summaries
-        for (let i = 0; i < 10; i++) {
-          result.then(function (data) {
-            var urlForPlotSummary = "https://www.omdbapi.com/?i=" + data.Search[i].imdbID + "&apikey=640d2cde";
-            return fetch(urlForPlotSummary);
-          })
-          .then( function(response) {
-            return response.json();
-          })
-          // Add plot summaries to html
-          .then( function(data) {
-              document.getElementById("text"+i+"").innerText = data.Plot;
-              var genreClasses = document.getElementsByClassName("genre");
-              genreClasses[i].innerText = data.Genre;
-          });
-        }
+		butN.addEventListener("click", function() {if (page<100) {page++;restoreButtonsIfGone(); search(title); }});
 
-
-        document.getElementById("button0").addEventListener("click", function() { more(0); });
-        document.getElementById("button1").addEventListener("click", function() { more(1); });
-        document.getElementById("button2").addEventListener("click", function() { more(2); });
-        document.getElementById("button3").addEventListener("click", function() { more(3); });
-        document.getElementById("button4").addEventListener("click", function() { more(4); });
-        document.getElementById("button5").addEventListener("click", function() { more(5); });
-        document.getElementById("button6").addEventListener("click", function() { more(6); });
-        document.getElementById("button7").addEventListener("click", function() { more(7); });
-        document.getElementById("button8").addEventListener("click", function() { more(8); });
-        document.getElementById("button9").addEventListener("click", function() { more(9); });
-
-    }
+	}
 }
 
-async function more( num ) {
-  var title = document.getElementById("submit").value;
-  var urlForTitleAndPoster = "https://www.omdbapi.com/?s=" + title +"&page=5&apikey=640d2cde";
+function search(t)
+{
+	var urlForTitleAndPoster = "https://www.omdbapi.com/?s=" + t +"&page="+ page +"&apikey=640d2cde";
 
-  getData(urlForTitleAndPoster)
-    .then( function(data) {
-      var urlForFullPlotSummary = "https://www.omdbapi.com/?i=" + data.Search[num].imdbID + "&plot=full&apikey=640d2cde";
-      return getData(urlForFullPlotSummary);
-    })
-    .then( function(data){
-      document.getElementById("text"+num).innerText = data.Plot;
-    });
-    var element = document.getElementById("button"+num+"");
-    element.parentNode.removeChild(element);
-}
+	if(page>=1)
+	{
+		document.getElementById("page").innerHTML="Page "+page;
+	}
+	if(page==1 )
+	{
+		document.getElementById("previous").disabled = true;
+	}
+	else
+	{
+		document.getElementById("previous").disabled = false;
+	}
+	if(page==100 )
+	{
+		document.getElementById("next").disabled = true;
+	}
+	else
+	{
+		document.getElementById("next").disabled = false;
+	}
 
-async function getData(url){
-  let response = await fetch(url);
-
-  let data = await response.json();
-
-  return data;
-}
-
-function restoreButtonsIfGone(){
-  var boxClasses = document.getElementsByClassName("box");
-  for (var i =0 ; i < 10 ; i++){
-    if(! $("#button"+i+"").length){  $(boxClasses[i]).append($("<button>More</>").attr("id", "button"+i+"").attr("value", i).addClass("button")); }
-  }
-}
-
-function addElement(parentId, elementTag, elementId, html) {
-    // Adds an element to the document
-    var p = document.getElementById(parentId);
-    var p = document.getElementsByClassName(box);
-    var newElement = document.createElement(elementTag);
-    newElement.setAttribute('id', elementId);
-    newElement.innerHTML = html;
-    p.appendChild(newElement);
-}
-
-function addResponseToDivsInFrontEnd(hasSearched){
-  if (!hasSearched){
-    for (var i = 0; i < 10; i++){
-        $("#myContainer").append(
-          $("<div/>").addClass("container").append(
-            $("<div/>").addClass("box").append(
-              $("<figure/>").addClass("myimg").append(
-                $('<img src="http://placehold.it/250x200"/>').addClass("poster")
-
-              )
-              )
-              .append($("<h3/>").addClass("titleShow"))
-              .append($("<p/>").attr("id", "text"+i+""))
-               .append($("<span/>").addClass("released"))
-               .append($("<span/>").addClass("genre"))
-               .append($("<button>More</>").attr("id", "button"+i).attr("value", i).addClass("button"))
-
-
-          )
-        )
-    }
+  for(let i=0;i<10;i++)
+  {
+    document.getElementById("button"+i).addEventListener("click", function() { more(i); });
   }
 
+
+	// Retrieve json query
+	var result = getData(urlForTitleAndPoster)
+	.then( function(data) {
+		console.log(data);console.log(page);
+		console.log(data.Response);
+    if (data.Response === "False"){
+      alert("No results found");
+    }
+		console.log(data.totalResults);
+
+		var releasedClasses = document.getElementsByClassName("released");
+		var titleShowClasses = document.getElementsByClassName("titleShow");
+		var posterClasses = document.getElementsByClassName("poster");
+
+
+		for (var i = 0; i < 10; i++)
+		{
+			// Add Title and Poster to html
+			titleShowClasses[i].innerText = data.Search[i].Title;
+			posterClasses[i].src = data.Search[i].Poster;
+			releasedClasses[i].innerHTML = "<strong>Released:</strong> " + data.Search[i].Year;
+
+		}
+
+		return data;
+
+	});
+
+	// Retrieve plot summaries
+	for (let i = 0; i < 10; i++)
+	{
+		result.then(function (data)
+		{
+			var urlForPlotSummary = "https://www.omdbapi.com/?i=" + data.Search[i].imdbID + "&apikey=640d2cde";
+			return fetch(urlForPlotSummary);
+		})
+
+		.then( function(response)
+		{
+			return response.json();
+		})
+
+		// Add plot summaries to html
+		.then( function(data)
+		{
+			document.getElementById("text"+i+"").innerText = data.Plot;
+			var genreClasses = document.getElementsByClassName("genre");
+			var director = document.getElementsByClassName("director");
+			var actors = document.getElementsByClassName("actors");
+			genreClasses[i].innerText = data.Genre;
+			director[i].innerHTML = "<strong>Director:</strong> " + data.Director;
+			actors[i].innerHTML = "<strong>Actors:</strong> " + data.Actors;
+		});
+	}
+}
+
+async function more( num )
+{
+	var title = document.getElementById("submit").value;
+	var urlForTitleAndPoster = "https://www.omdbapi.com/?s=" + title +"&page="+ page +"&apikey=640d2cde";
+
+	getData(urlForTitleAndPoster)
+
+	.then( function(data) {
+	var urlForFullPlotSummary = "https://www.omdbapi.com/?i=" + data.Search[num].imdbID + "&plot=full&apikey=640d2cde";
+	return getData(urlForFullPlotSummary);
+	})
+
+	.then( function(data){
+	document.getElementById("text"+num).innerText = data.Plot;
+	});
+
+	var element = document.getElementById("button"+num+"");
+	element.parentNode.removeChild(element);
+}
+
+async function getData(url)
+{
+	let response = await fetch(url);
+
+	let data = await response.json();
+
+	return data;
+}
+
+function restoreButtonsIfGone()
+{
+	var boxClasses = document.getElementsByClassName("box");
+
+	for (var i =0 ; i < 10 ; i++)
+	{
+		if(! $("#button"+i+"").length)
+		{
+			$(boxClasses[i]).append($("<button>More</>").attr("id", "button"+i+"").attr("value", i).addClass("button"));
+		}
+	}
+}
+
+function addResponseToDivsInFrontEnd(hasSearched)
+{
+	if (!hasSearched)
+	{
+		for (var i = 0; i < 10; i++)
+		{
+			$("#myContainer").append(
+				$("<div/>").addClass("container").append(
+					$("<div/>").addClass("box").append(
+						$("<figure/>").addClass("myimg").append(
+							$('<img src="http://placehold.it/250x200"/>').addClass("poster")
+
+						)
+
+					)
+					.append($("<h3/>").addClass("titleShow"))
+					.append($("<p/>").attr("id", "text"+i+""))
+					.append($("<button>More</>").attr("id", "button"+i).attr("value", i).addClass("button"))
+					.append($("<div/>").addClass("info").append(
+						$("<p/>").addClass("director"))
+						.append($("<p/>").addClass("actors"))
+						.append($("<p/>").addClass("released"))
+						.append($("<p/>").addClass("genre"))
+					)
+				)
+			).append($("<br/>"))
+		}
+	}
 }
